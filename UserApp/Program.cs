2303d4ba2dc9c1c573fd8cbd1a11bcbfa2d8f7b8
Oleksandr Rohan <o.rohanov@gmail.com>
2024-05-03 12:16:20 +0300

    added two instances User1 and User2

--- UserApp/Program.cs
+++ UserApp/Program.cs
@@ -6,5 +6,19 @@ using UserApp;
 var user1 = new User("Oleksandr", "Rohanov", GenderType.Male);
 Console.WriteLine(user1);
 
+var user2 = new User
+{
+    Age = 8
+};
+Console.Write("Enter First Name: ");
+user2.FirstName = Console.ReadLine();
 
+Console.Write("Enter Last Name: ");
+user2.LastName = Console.ReadLine();
+
+Console.Write("Enter gender (Male, Female або Unknown): ");
+string str = Console.ReadLine();
+user2.EnterGender(str);
+
+Console.WriteLine(user2);
 
